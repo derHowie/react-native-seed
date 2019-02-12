cded1d055f659e179a01ebfa5378009f6c16695b
Christopher Howard <derHowieDesigns@gmail.com>
2019-02-11 21:45:19 -0500

    install ts

---
+++ rn-cli.config.js
@@ -0,0 +1,8 @@
+modules.exports = {
+    getTransformModulePath() {
+        return require.resolve("react-native-typescript-transformer");
+      },
+      getSourceExts() {
+        return ["ts", "tsx"];
+      },
+};
